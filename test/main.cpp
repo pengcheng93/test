1c7f9ca7241fb06fcf24960fe9948022458c5ded
visgroup <visgroup@visgroupdemacbook-pro.local>
2018-03-16 16:43:40 +0800

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by visgroup on 2018/3/16.
+//  Copyright © 2018年 visgroup. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
